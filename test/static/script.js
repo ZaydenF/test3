a5056ab00719ca3df0ae593112afed7d403e0e02
ZaydenF <zaydenfleming@gmail.com>
2025-01-23 10:48:34 -0800

    Changed Source Folder

--- test/static/script.js
+++
@@ -1,9 +0,0 @@
-  $(document).ready(function() {
-            $(".cards").click(function() {
-                var $remainingButtons = $(".cards:visible");
-                
-                if ($remainingButtons.length > 0) {
-                    $remainingButtons.first().hide();
-                }
-            });
-        });
